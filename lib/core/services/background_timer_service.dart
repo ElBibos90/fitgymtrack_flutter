dc04a02cb974f3dab04279bf5eedc61de9f2b51f
ElBibos90 <166718717+ElBibos90@users.noreply.github.com>
2025-08-05 11:19:23 +0200

    Remove unused timezone import
    Eliminated the unused import of 'package:timezone/timezone.dart' as tz from background_timer_service.dart to clean up the code.

--- lib/core/services/background_timer_service.dart
+++ lib/core/services/background_timer_service.dart
@@ -9,7 +9,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 import '../di/dependency_injection.dart';
 import 'audio_settings_service.dart';
